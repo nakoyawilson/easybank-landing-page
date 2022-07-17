6d283d5bd7a3e6714cc23ce6929de84aac7d595a
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-07-17 09:29:34 -0400

    Add structure to page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/main.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "hsl(233, 26%, 24%)",
+        "lime-green": "hsl(136, 65%, 51%)",
+        "bright-cyan": "hsl(192, 70%, 51%)",
+        "grayish-blue": "hsl(233, 8%, 62%)",
+        "light-grayish-blue": "hsl(220, 16%, 96%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
